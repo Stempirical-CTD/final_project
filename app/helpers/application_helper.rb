928f6f13abbc904d4d0c9a1410a782684e981873
Aaron Wiggins <apw7177@gmail.com>
2015-07-03 18:52:28 -0400

    instructions added to experiment show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-  def order_number(experiment)
-    if experiment.instruction.count == 0
-      instruction.order == 1
-    else
-      instruction.order = instructions.count + 1
-    end
-  end
 end
