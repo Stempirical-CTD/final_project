ebf60204984c2d738c48885ba957e7d6fed812de
Aaron Wiggins <apw7177@gmail.com>
2015-07-08 20:28:31 -0400

    delete lessons from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,6 @@ user_two = User.create!(email: "user@usertwo.com", username: "usertwo", password
 experiment = Experiment.create!(user_id: user.id, description: "How do scientists take DNA out
     of a living thing? It's not that hard—there are lots of ways to do it! You
     can follow the steps below. Either way you'll have strawberry DNA at the end!",
-    # lesson: "Strawberries, bacteria, humans—all living things have genes, and
-    #     all of these genes are made of DNA. That's why scientists can take a
-    #     gene from one living thing and put it into another.",
     youtube_link: "https://www.youtube.com/watch?v=67KXatgoNKs", complete_time: 1.5,
     name: "Extracting Strawberry DNA")
 
@@ -38,7 +35,6 @@ experiment_two = Experiment.create!(user_id: user_two.id, name: "How To Make A S
     description: " A lightning bolt is basically a dramatic display of static electricity
         in action. You see lightning when a spark of moving electrons races up or
         down between a cloud and the ground.",
-    # lesson: "demonstrates static electricity",
     youtube_link: "https://www.youtube.com/watch?v=XtE0uXGMC-0", complete_time: 1)
 
     mat_four= Material.create!(experiment_id: experiment_two.id, item: "pencil with erasser")
