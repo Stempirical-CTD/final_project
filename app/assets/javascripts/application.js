ad689a020df29dbb0bc6651fca0bde24834b23c3
Lindsey Miller <lindc.miller@gmail.com>
2015-07-14 18:31:58 -0400

    set up for nav-link active style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,19 +21,10 @@
 //     $(this).css("color", "red");
 // });
 
-// create experiment view: tab navigation
+// navigation link - stay active
 
-$(document).ready(function(){
-    $("ul#tabs li").click(function(e){
-        if (!$(this).hasClass("active")) {
-            var tabNum = $(this).index();
-            var nthChild = tabNum+1;
-            $("ul#tabs li.active").removeClass("active");
-            $(this).addClass("active");
-            $("ul#tab li.active").removeClass("active");
-            $("ul#tab li:nth-child("+nthChild+")").addClass("active");
-        }
-    });
+$('.nav-link').click(function(){
+   $(this).toggleClass('.active');
 });
 
 // create experiment view: add fields dynamically
