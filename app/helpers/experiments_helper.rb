4f75341aceba30b288b5654541bff84201b8b07a
Aaron Wiggins <apw7177@gmail.com>
2015-07-20 16:44:05 -0400

    fix checkbox selection

--- app/helpers/experiments_helper.rb
+++ app/helpers/experiments_helper.rb
@@ -6,6 +6,6 @@ module ExperimentsHelper
   end
 
   def find_unique_experiment(exp_one, exp_two, exp_three)
-    Experiment.where.not(name: [exp_one, exp_two, exp_three]).by_votes# AND name != ? AND name != ?", q_one, q_two, q_three)
+    Experiment.where.not(name: [exp_one, exp_two, exp_three]).by_votes.first# AND name != ? AND name != ?", q_one, q_two, q_three)
   end
 end
