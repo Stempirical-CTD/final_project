40c30faad0cc61ff5e83c1b5008d42960039bbd7
Andy Andrea <andy.andrea@scimedsolutions.com>
2017-06-21 22:44:08 -0400

    Remove unused code

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,5 @@
 class CommentsController < ApplicationController
   before_filter :authenticate_user!
-  # before_action :set_comment, only: [:show, :edit, :create, :update, :destroy]
 
   def create
     @comment = @commentable.comments.new(comment_params)
@@ -11,30 +10,8 @@ class CommentsController < ApplicationController
       format.js
     end
   end
-  #
-  # def edit
-  #
-  # end
-  #
-  # def update
-  #   if @comment.update(comment_params)
-  #     redirect_to @commentable, notice: 'Comment was successfully updated.'
-  #   else
-  #     render :edit
-  #   end
-  # end
-  #
-  # def destroy
-  #   @comment.destroy
-  #   redirect_to @commentable, notice: 'Comment was successfully destroyed.'
-  # end
-  #
 
   private
-
-  # def set_comment
-  #   @comment = Comment.find(params[:id])
-  # end
 
   def comment_params
     params.require(:comment).permit(:body, :id)
--- app/models/material.rb
+++ app/models/material.rb
@@ -1,5 +1,4 @@
 class Material < ActiveRecord::Base
   belongs_to :experiment
   validates :item, presence: true
-  # validates :item, uniqueness: { scope: :experiment_id }
 end
